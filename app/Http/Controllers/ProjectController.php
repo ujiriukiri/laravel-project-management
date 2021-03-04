0a0953ff9404374d833085bbadcb45dc22f46720
Ujiri Ukiri <ujiri.ukiri@ymail.com>
2021-03-05 00:06:11 +0100

    feat: Add projects index table

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -14,10 +14,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        return response()->json([
-            'status' => 'success',
-            'data' => Project::with('issues')->get()
-        ]);
+        $projects = Project::with('issues')->get();
+        return view('projects.index', ['projects' => $projects]);
     }
 
     /**
