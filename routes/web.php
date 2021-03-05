b2835ea3a2e47badc8879f60dea2430e8369380a
Ujiri Ukiri <ujiri.ukiri@ymail.com>
2021-03-05 03:35:17 +0100

    chore: Clean web routes

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__.'/auth.php';
 
 Route::group(['middleware' => 'auth'], function(){
