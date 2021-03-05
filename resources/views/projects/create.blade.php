a447e47601be0555e6ca7e2928fd2e5d83a94b0b
Ujiri Ukiri <ujiri.ukiri@ymail.com>
2021-03-05 03:33:38 +0100

    feat: Update project create view for edit also

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -7,15 +7,18 @@
 
     <div class="py-12 xs:px-4">
         <div class="max-w-3xl mx-auto px-4 lg:px-8">
-            <form action="{{ route('projects.store') }}" method="POST">
+            <form action="{{ request()->routeIs('projects.create') ? route('projects.store') : route('projects.update', $project->id) }}" method="POST">
                 @csrf
+                @if (!request()->routeIs('projects.create'))
+                    <input type="hidden" name="_method" value="PUT">
+                @endif
                 <div class="border-t border-b border-gray-300 py-8">
                     <div class="md:w-2/3 w-full mb-6">
                         <label for="title" class="block text-sm font-bold text-gray-700">
                             Project Title
                         </label>
                         <div class="mt-1 flex rounded-md shadow-sm">
-                            <input type="text" name="title" id="title"
+                            <input type="text" name="title" id="title" value="{{ $project->title }}"
                                 class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300"
                                 placeholder="Project Title">
                         </div>
@@ -26,14 +29,14 @@
                         </label>
                         <div class="mt-1">
                             <textarea id="description" name="description" rows="7"
-                                class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
+                                class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md">{{ $project->description }}</textarea>
                         </div>
                     </div>
                 </div>
                 <div class="mt-6 sm:mt-4">
                     <button type="submit"
                         class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Create Project
+                        {{ request()->routeIs('projects.create') ? __('Create') : __('Save') }} {{ __('Project') }}
                     </button>
                 </div>
             </form>
